9be4a8bddcc199d0a849d4bb04523eda49059b82
Michael Lieschnegg <lieschnegg@lilatec.com>
2022-10-20 11:06:02 +0200

    Vorlage Node: Steuerung des TurtleBot

--- src/robu/robu/remotectrl_sus.py
+++ src/robu/robu/remotectrl_sus.py
@@ -20,12 +20,6 @@ Date:       ?
 e = """
 Communications Failed
 """
-#75...K links
-#77...M rechts
-#72...H vorwärts
-#80...P rückwärts
-#27...Escape
-key_ctrl = [75, 77, 72, 80]
 
 def get_key():
     return msvcrt.getch().decode('utf-8')
@@ -38,28 +32,7 @@ def main():
     pub = node.create_publisher(Twist, 'cmd_vel', qos)
 
     try:
-        print(msg)
-        #add Code here
-        while(1):
-            key = get_key();
-            print("ASCII Zeichen", key, "Code: ", ord(key))
-
-            if ord(key) == 0x00:
-                key_null_entered = True
-            elif ord(key) == 27: #Escape wurde gedrueckt -> Roboter sollte stehen bleiben
-                print("Escape")
-            elif ord(key) == 3: #STRG+C
-                break
-            elif key_null_entered == True and ord(key) in key_ctrl:
-                if key_ctrl[0] == ord(key):     #Links
-                    print("Links")
-                elif key_ctrl[1] == ord(key):   #Rechts
-                    print("Rechts")
-                elif key_ctrl[2] == ord(key):   #Vorwärts
-                    print("Vorwärts")
-                elif key_ctrl[3] == ord(key):   #Rückwärts
-                    print("Rückwärts")
-                key_null_entered = False
+        pass
                 
     except Exception as e:
         print(e)
